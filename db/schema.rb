674b3ca8695ff00f538a08b6d1d08d2648371ac5
CJ May <cmtechboy1@gmail.com>
2020-02-16 13:00:43 -0500

    fixed articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919031503) do
+ActiveRecord::Schema.define(version: 20200216175508) do
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
